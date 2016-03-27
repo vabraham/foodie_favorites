1491e633b6f3f5078386169c373db3ed5198c09c
Vineet Abraham <vabraham24@gmail.com>
2016-03-27 15:39:37 -0700

    updated web app files

--- web_app/proj.py
+++ web_app/proj.py
@@ -41,58 +41,51 @@ app = Flask(__name__)
 # Set "homepage" to index.html
 @app.route('/')
 def index():
-    conn = psycopg2.connect(database='sample_proj', user='vabraham24', host="/tmp/")
+    conn = psycopg2.connect(database='yelp', user='postgres', password='testing1')
     c = conn.cursor()
     c.execute("""   SELECT DISTINCT(city)
             FROM restaurants
             ORDER BY city; """)
     cities = c.fetchall()
-    # c.execute("""   SELECT DISTINCT(rest_name)
-    #         FROM restaurants
-    #         ORDER BY rest_name; """)
-    # rests = c.fetchall()
     conn.close()
     sample_cities = [x[0] for x in cities]
-    # sample_rests = [x[0] for x in rests]
     return render_template('index.html', sample_cities=sample_cities)
-    # , sample_rests=sample_rests
 
-# Save e-mail to database and send to success page
+
 @app.route('/city/<city_name>', methods=['GET','POST'])
 def city(city_name):
     # return "test"
     # return request.form['item']
-    name = city_name
+    city_name = city_name
     # name = str(name)
     # return restaurant_name
     # if request.method == 'GET':
-    conn = psycopg2.connect(database='sample_proj', user='vabraham24', host="/tmp/")
+    conn = psycopg2.connect(database='yelp', user='postgres', password='testing1')
     c = conn.cursor()
     c.execute("""   SELECT DISTINCT(rest_name)
         FROM restaurants
         WHERE city = %s
-        ORDER BY rest_name; """, (name,))
+        ORDER BY rest_name; """, (city_name,))
     rests = c.fetchall()
     conn.close()
     sample_rests = [x[0] for x in rests]
-    return render_template('restaurants.html', sample_rests=sample_rests)
-    # , name=name
-    # return render_template('index.html')
+    return render_template('restaurants.html', sample_rests=sample_rests, city_name=city_name)
 
 
-# Save e-mail to database and send to success page
-@app.route('/restaurant/<restaurant_name>', methods=['GET', 'POST'])
-def restaurant(restaurant_name):
+@app.route('/restaurant/<city_name>/<restaurant_name>', methods=['GET', 'POST'])
+def restaurant(restaurant_name, city_name):
     name = restaurant_name
+    city = city_name
     zingers = ["Bingo!", "Yum!", "And the votes are in...", "De-licious"]
     phrase = sample(zingers, 1)[0]
-    conn = psycopg2.connect(database='sample_proj', user='vabraham24', host="/tmp/")
+    conn = psycopg2.connect(database='yelp', user='postgres', password='testing1')
     c = conn.cursor()
     c.execute("""   SELECT menu_item
         FROM restaurants
         WHERE rest_name = %s
+        AND city = %s
         ORDER BY count DESC
-        LIMIT 10    ; """, (name,))
+        LIMIT 10    ; """, (name, city))
     menu = c.fetchall()
     conn.close()
     menu_items = [x[0] for x in menu]
@@ -101,4 +94,4 @@ def restaurant(restaurant_name):
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=8090)
+    app.run(host='0.0.0.0', port=80)
